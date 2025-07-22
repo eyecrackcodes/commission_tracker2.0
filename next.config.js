e7b31790ad1b10099c4deb4647f4509c7eaa8fad
eyecrackcodes <eyecrackcodes@gmail.com>
2025-07-22 15:12:39 -0500

    Fix Next.js Image component: Add img.clerk.com to allowed domains

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["images.clerk.dev"],
+    domains: ["images.clerk.dev", "img.clerk.com"],
   },
   env: {
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
